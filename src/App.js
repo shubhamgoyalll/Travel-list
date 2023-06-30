148c29667ac8d97124851a2ab6972c466bf78d44
Shubham Goyal <goyalshubham2521@gmail.com>
2023-07-01 00:55:30 +0530

    Create:sortingitembutton

--- src/App.js
+++ src/App.js
@@ -85,10 +85,30 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToogleItem }) {
+  //using controlled element concept
+  const [sortBy, setSortBy] = useState("input");
+
+  //using derived state concept
+  let sortedItems;
+
+  //As by default item list would be input type
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    //a.description.localeCompare(b.description) - to compare first and second item in a list by alphabitcal order
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -97,6 +117,15 @@ function PackingList({ items, onDeleteItem, onToogleItem }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        {/* select has value and onChange which is 2nd and 3rd step for controlled elements concept */}
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by Input Order</option>
+          <option value="description">Sort by Description</option>
+          <option value="packed">Sort by Packed Status</option>
+        </select>
+      </div>
     </div>
   );
 }
